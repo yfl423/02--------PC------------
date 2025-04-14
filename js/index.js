9f3e39c286c9f0380548e0fd841af73612602e5e
yfl423 <yfl423@tamu.edu>
2025-04-14 15:32:29 -0500

    Added mask element and make it controllable with mouse

--- js/index.js
+++ js/index.js
@@ -1,27 +1,81 @@
-window.onload = function(){
-    let navPath = document.getElementsByClassName("navPath")[0];
-    let pathData = goodData.path;
+window.onload = function () {
+    navPathDataRender();
+    bigPictureBind();
 
-    // console.log(pathData);
-    // console.log(navPath);
+    /**
+     * 
+     * @returns Render the navigation path of the product page.
+     */
+    function navPathDataRender() {
+        let navPath = document.getElementsByClassName("navPath")[0];
+        let pathData = goodData.path;
 
-    for(let i = 0; i < pathData.length; i++){
-        if(i == pathData.length -1){
+        // console.log(pathData);
+        // console.log(navPath);
+
+        for (let i = 0; i < pathData.length; i++) {
+            if (i == pathData.length - 1) {
+                let data = pathData[i];
+                let aNode = document.createElement("a");
+                aNode.innerText = data.title;
+                navPath.appendChild(aNode);
+                return;
+            }
+
+
             let data = pathData[i];
             let aNode = document.createElement("a");
             aNode.innerText = data.title;
+            aNode.href = data.url;
+            let iNode = document.createElement("i");
+            iNode.innerText = '/';
             navPath.appendChild(aNode);
-            return;
+            navPath.appendChild(iNode);
         }
-
-
-        let data = pathData[i];
-        let aNode = document.createElement("a");
-        aNode.innerText = data.title;
-        aNode.href = data.url;
-        let iNode = document.createElement("i");
-        iNode.innerText = '/';
-        navPath.appendChild(aNode);
-        navPath.appendChild(iNode);
     }
+
+
+    function bigPictureBind() {
+        let leftTop = document.getElementById("leftTop");
+        let smallPic = document.getElementById("smallPic");
+        console.log(smallPic);
+    
+        let mask;
+        let bigPic;
+    
+        smallPic.onmouseenter = function () {
+            // Create mask
+            mask = document.createElement("div");
+            mask.className = "mask";
+            smallPic.appendChild(mask);
+    
+            bigPic = document.createElement("div");
+            bigPic.id = "bigPic";
+            let bigPicImg = document.createElement("img");
+            bigPicImg.src = "images/b1.png";
+            bigPic.appendChild(bigPicImg);
+            leftTop.appendChild(bigPic);
+        };
+
+
+        // Move the mask and bigPic
+        // The mask is moved according to the mouse position, and the bigPic is moved according to the mask position
+        smallPic.onmousemove = function (event){
+            let left  = event.clientX - smallPic.getBoundingClientRect().left - mask.offsetWidth / 2;
+            let top = event.clientY - smallPic.getBoundingClientRect().top - mask.offsetHeight / 2;
+            mask.style.left = left + "px";
+            mask.style.top = top + "px";
+        }
+    
+        smallPic.onmouseleave = function () {
+            // Delete mask & bigPic
+            if (mask) {
+                smallPic.removeChild(mask);
+            }
+            if (bigPic) {
+                leftTop.removeChild(bigPic);
+            }
+        };
+    }
+
 }
